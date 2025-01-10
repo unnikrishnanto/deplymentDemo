881d440a70c1e6f1922ec438345af106617a7bfb
Unnikrishnan <unnikrishnantoa@gmail.com>
2025-01-10 21:52:16 +0530

    added Buttons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
         <h1>Kodnest</h1>
         <Classes name = 'Java' tutor = 'Punith' />
         <Classes name = 'Python' tutor = 'Deep' />
+        <button>Button 1</button>
+        <button>Button 2</button>
       </div>
       );
 }
